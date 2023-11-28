ff60c5534a3566972d4ea6cc6014ae4b0ea689e3
Abel <aatsou12@gmail.com>
2023-11-28 17:59:14 +0000

    file added

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,5 @@
- script to install Flask
+#script to install Flask
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-}
-
-package { 'werkzeug':
-  ensure   => '2.1.1',
   provider => 'pip3',
 }
