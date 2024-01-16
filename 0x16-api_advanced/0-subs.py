83dd1aad8386b638defc930f71dc068a5260d8ce
Abel <aatsou12@gmail.com>
2024-01-16 17:40:06 +0000

    done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,10 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux request "
-    }
-    response = requests.get(url, headers=headers, allow_redirects=True)
+    response = requests.get(url, allow_redirects=True)
     if response.status_code == 404:
         return 0
     results = response.json().get("data")
