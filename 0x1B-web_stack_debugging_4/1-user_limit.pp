f32196fd77d0f892ac5003cac4b1fcfaf99db3d9
Abel <aatsou12@gmail.com>
2024-01-25 14:49:40 +0000

    done

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+# fix limit file at hbton user.
+
+exec { 'fix_limitis_hton_':
+  command => 'sed -i "/holberton hard/s/5/10000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase soft file limit hbton ser.
+exec { 'increase_asoft_file':
+  command => 'sed -i "/holberton soft/s/4/20000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
